ae04f8eb2119932366caddde88fba16ae51a0fef
amit <mr.ahr.es6@gmail.com>
2025-01-21 09:11:31 +0600

    get the title of child windows added

--- src/Test1.java
+++ src/Test1.java
@@ -1,5 +1,9 @@
+import java.util.Iterator;
+import java.util.Set;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,6 +12,8 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		// When we make small driver that is called Limiting WebDriver Scope
 
 		WebDriver driver = new ChromeDriver();
 
@@ -21,6 +27,34 @@ public class Test1 {
 		WebElement footerDriver = driver.findElement(By.id("gf-BIG"));
 		int links = footerDriver.findElements(By.tagName("a")).size();
 		System.out.println(links);
+
+		// Count all links in footer column
+		WebElement columnDriver = footerDriver.findElement(By.xpath("//tbody/tr/td[1]/ul[1]"));
+		int linkCount = columnDriver.findElements(By.tagName("a")).size();
+		System.out.println(linkCount);
+
+		for (int i = 0; i < linkCount; i++) {
+
+			// Chord for pressing many keys at once
+			String clickNewTab = Keys.chord(Keys.COMMAND, Keys.ENTER);
+			columnDriver.findElements(By.tagName("a")).get(i).sendKeys(clickNewTab);
+		}
+
+		Set<String> windows = driver.getWindowHandles();
+		Iterator<String> it = windows.iterator();
+		String parentWindow = it.next();
+		
+		while(it.hasNext()) {
+			String childWindow = it.next();
+			driver.switchTo().window(childWindow);
+			
+			String title =driver.getTitle();
+			
+			System.out.println(title);
+		}
+		
+
+		driver.close();
 	}
 
 }
